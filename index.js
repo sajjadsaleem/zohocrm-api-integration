3243b9ffad7dc92897fe83fde0de561a51c526ff
makbarsarwar <muhammad.akbar@barqsol.com>
2023-06-01 20:02:12 +0500

    updated

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const zohoCRMService = require('./service.zohocrm');
 
 const port = process.env.PORT;
 const hostname = process.env.HOSTNAME;
+const accessToken = process.env.ZOHO_ACCESS_TOKEN;
+const refreshToken = process.env.ZOHO_REFRESH_TOKEN;
 
 
 const server = http.createServer((req, res) => {
@@ -20,21 +22,64 @@ const server = http.createServer((req, res) => {
         console.log('Code:', code);
         // const getToken = zohoCRMService.getAccessToken(code)
         
-        const getToken = zohoCRMService.getAccessToken(code, function (error, token) {
-            if (error) {
-                console.error('Error getting access token:', error);
-            } else {
-                // console.log('getToken:', token.body.access_token);
-                res.end('code'+token.body.access_token);
-            }
-        });
+        if(accessToken != '' && refreshToken != ''){
+
+            res.statusCode = 302;
+            res.setHeader('Content-Type', 'text/plain');
+            res.setHeader('Location', '/'); // Set the URL to redirect to
+            res.end();
+
+        } else {
+
+            zohoCRMService.getAccessToken(code, function (error, token) {
+                if (error) {
+                    console.error('Error getting access token:', error);
+                } else {
+                    // console.log('getToken:', token.body.access_token);
+                    res.end('Kindly update this ZOHO_ACCESS_TOKEN='+token.body.access_token+' and ZOHO_REFRESH_TOKEN='+token.body.refresh_token+' in your .env file.');
+                }
+            });
+
+        }
+        
 
 
     } else {
-        res.statusCode = 302;
-        res.setHeader('Content-Type', 'text/plain');
-        res.setHeader('Location', zohoCRMService.getCRMURL()); // Set the URL to redirect to
-        res.end();
+        if(accessToken != '' && refreshToken != ''){
+            
+            const parsedUrl = url.parse(req.url);
+            const queryParameters = querystring.parse(parsedUrl.query);
+
+            if (queryParameters.email) {
+                zohoCRMService.getRefreshToken(function (error, token) {
+                    if (error) {
+                        console.error('Error getting access token:', error);
+                    } else {
+                        // console.log('ZOHO_ACCESS_TOKEN=', accessToken+' & ZOHO_REFRESH_TOKEN='+refreshToken+' available');
+                        const formData = {
+                            Email: queryParameters.email,
+                            Last_Name: queryParameters.last_name,
+                        }
+                        // console.log('req=',formData)
+                        zohoCRMService.addContact(token.body.access_token,formData, function (error, contactRes) {
+                            if (error) {
+                                console.error('Error getting access token:', error);
+                            } else {
+                                res.end(contactRes.message);
+                            }
+                        });
+                    }
+                });
+            } else {
+                res.end('email and last_name field is required');
+            }
+
+        } else {
+            res.statusCode = 302;
+            res.setHeader('Content-Type', 'text/plain');
+            res.setHeader('Location', zohoCRMService.getCRMURL()); // Set the URL to redirect to
+            res.end();
+        }
     }
 
 });
--- service.zohocrm.js
+++ service.zohocrm.js
@@ -5,6 +5,8 @@ const baseURL = process.env.BASE_URL;
 const clientId = process.env.ZOHO_CLIENT_ID;
 const clientSecret = process.env.ZOHO_CLIENT_SECRET;
 const baseUrl = process.env.BASE_URL;
+const accessToken = process.env.ZOHO_ACCESS_TOKEN;
+const refreshToken = process.env.ZOHO_REFRESH_TOKEN;
 
 // CREATE URL
 function getCRMURL() {
@@ -41,33 +43,102 @@ function getAccessToken(code, callback) {
           return callback(error); // Pass the error to the callback if it's a function
         }
       }
+      
+      var token = {
+        'status': true,
+        'message': 'success',
+        'body': JSON.parse(body)
+      };
+
+      if (typeof callback === 'function') {
+        return callback(null, token); // Pass the token to the callback if it's a function
+      }
+
+    });
+}
+
+// GET REFRESH TOKEN
+function getRefreshToken(callback) {
+    var data = {
+      refresh_token: refreshToken,
+      client_id: clientId,
+      client_secret: clientSecret,
+      grant_type: 'refresh_token'
+    };
   
-      fs.writeFile(process.env.ZOHO_TOKEN_FILE, body, (err) => {
-        if (err) {
-          console.error('Error writing JSON file:', err);
-          if (typeof callback === 'function') {
-            return callback(err); // Pass the error to the callback if it's a function
-          }
-        }
+    var options = {
+      'method': 'POST',
+      'url': 'https://accounts.zoho.com/oauth/v2/token',
+      formData: data
+    };
   
-        console.log('Data saved to data.json');
-  
-        var token = {
-          'status': true,
-          'message': 'success',
-          'body': JSON.parse(body)
-        };
-  
+    request(options, function (error, response, body) {
+      if (error) {
+        console.error('Error:', error);
         if (typeof callback === 'function') {
-          return callback(null, token); // Pass the token to the callback if it's a function
+          return callback(error); // Pass the error to the callback if it's a function
         }
-      });
+      }
+
+      console.log('JSON.parse(body)=',JSON.parse(body))
+      
+      var token = {
+        'status': true,
+        'message': 'success',
+        'body': JSON.parse(body)
+      };
+
+      if (typeof callback === 'function') {
+        return callback(null, token); // Pass the token to the callback if it's a function
+      }
+
+    });
+}
+
+// ADD CONTACT
+function addContact(token,formData, callback) {
+    var options = {
+      'method': 'POST',
+      'url': 'https://www.zohoapis.com/crm/v4/Contacts',
+      'headers': {
+        'Authorization': 'Zoho-oauthtoken '+token,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        "data": [
+          formData
+        ]
+      })
+    };
+  
+    request(options, function (error, response, body) {
+      if (error) {
+        console.error('Error:', error);
+        if (typeof callback === 'function') {
+          return callback(error); // Pass the error to the callback if it's a function
+        }
+      }
+
+      const resData = JSON.parse(body)
+      console.log('addContact resData=',resData.data[0].message)
+      
+      var data = {
+        'status': true,
+        'message': resData.data[0].message+' '+formData.Email,
+        'body': resData
+      };
+
+      if (typeof callback === 'function') {
+        return callback(null, data);
+      }
+
     });
 }
   
-  // Export the functions to make them accessible from other files
+// Export the functions to make them accessible from other files
 module.exports = {
     getCRMURL,
-    getAccessToken
+    getAccessToken,
+    getRefreshToken,
+    addContact
 };
-  
